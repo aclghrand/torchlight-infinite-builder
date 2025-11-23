ec8d517891d63f3c57a4bdcdd108b7bf2bd953b8
a <aclghrand@gmail.com>
2025-11-22 19:42:08 -0800

    cast speed

--- src/tli/stuff.ts
+++ src/tli/stuff.ts
@@ -1,7 +1,8 @@
 import * as R from "remeda";
 import { match } from "ts-pattern";
 import * as Affix from "./affix";
-import { DmgModType, CritRatingModType, CritDmgModType } from "./constants";
+import { Mod } from "./mods";
+import { DmgModType } from "./constants";
 
 let dummy40Armor = 0.11;
 let dummy85Armor = 0.44;
@@ -118,57 +119,6 @@ export interface Loadout {
   divinityPage: DivinityPage;
   customConfiguration: Affix.Affix[];
 }
-
-type Mod =
-  | {
-      type: "Str";
-      value: number;
-      mod: "pct" | "flat";
-      src?: string;
-    }
-  | {
-      type: "Dex";
-      value: number;
-      mod: "pct" | "flat";
-      src?: string;
-    }
-  | {
-      type: "Int";
-      value: number;
-      mod: "pct" | "flat";
-      src?: string;
-    }
-  | {
-      type: "DmgPct";
-      value: number;
-      addn: boolean;
-      modType: DmgModType;
-      src?: string;
-    }
-  | {
-      type: "CritRatingPct";
-      value: number;
-      modType: CritRatingModType;
-      src?: string;
-    }
-  | {
-      type: "CritDmgPct";
-      value: number;
-      addn: boolean;
-      modType: CritDmgModType;
-      src?: string;
-    }
-  | {
-      type: "AspdPct";
-      value: number;
-      addn: boolean;
-      src?: string;
-    }
-  | {
-      type: "FervorEffPct";
-      value: number;
-      src?: string;
-    };
 
 const calculateInc = (bonuses: number[]) => {
   return R.pipe(
