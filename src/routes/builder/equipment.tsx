91ca3b89d2291843f91b6542a2ba5ecbd25b4067
ericellb <ericellb@gmail.com>
2026-01-11 13:43:53 -0500

    fix: only i18n label

--- src/routes/builder/equipment.tsx
+++ src/routes/builder/equipment.tsx
@@ -170,7 +170,7 @@ function EquipmentPage(): React.ReactNode {
   const equipmentTypeOptions = useMemo(
     () =>
       allEquipmentTypes.map((type) => ({
-        value: i18n._(type),
+        value: type,
         label: i18n._(type),
       })),
     [allEquipmentTypes],
@@ -320,9 +320,9 @@ function EquipmentPage(): React.ReactNode {
     const sweet_dream_affix =
       sweetDreamAffixIndex !== undefined
         ? craft(
-            sweetDreamAffixes[sweetDreamAffixIndex],
-            sweetDreamAffixPercentage,
-          )
+          sweetDreamAffixes[sweetDreamAffixIndex],
+          sweetDreamAffixPercentage,
+        )
         : undefined;
 
     // Build tower sequence affix (1 max, no quality)
@@ -457,7 +457,7 @@ function EquipmentPage(): React.ReactNode {
                       percentage: 100,
                     }}
                     onAffixSelect={handleBaseStatsSelect}
-                    onSliderChange={() => {}}
+                    onSliderChange={() => { }}
                     onClear={handleClearBaseStats}
                     hideQualitySlider
                   />
@@ -522,7 +522,7 @@ function EquipmentPage(): React.ReactNode {
                       percentage: 100,
                     }}
                     onAffixSelect={handleTowerSequenceSelect}
-                    onSliderChange={() => {}}
+                    onSliderChange={() => { }}
                     onClear={handleClearTowerSequence}
                     hideQualitySlider
                   />
@@ -549,7 +549,7 @@ function EquipmentPage(): React.ReactNode {
                     }
                     selection={{ affixIndex: blendAffixIndex, percentage: 100 }}
                     onAffixSelect={handleBlendSelect}
-                    onSliderChange={() => {}}
+                    onSliderChange={() => { }}
                     onClear={handleClearBlend}
                     hideQualitySlider
                     formatOption={(affix) => {
